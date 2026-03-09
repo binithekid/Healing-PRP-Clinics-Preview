8fbe4ccaf3ad5990d34222716caa85638dbc3101
drsyedabdi-prog <dr.syedabdi@gmail.com>
2026-03-09 22:58:46 +0000

    Update layout.tsx

--- src/app/birmingham/p-shot/layout.tsx
+++ src/app/birmingham/p-shot/layout.tsx
@@ -4,9 +4,9 @@ import { ReactNode } from "react";
 export const metadata: Metadata = {
   title: {
     template: "%s | Birmingham Clinic",
-    default: "P-Shot® Male Rejuvenation | Birmingham Clinic",
+    default: "Doctor-Led P-Shot Male Rejuvenation | Birmingham Clinic",
   },
-  description: "Official P-Shot® (Priapus Shot) provider serving Edgbaston, Solihull & the West Midlands.",
+  description: "P-Shot (Priapus Shot) provider serving Edgbaston, Solihull & the West Midlands.",
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
