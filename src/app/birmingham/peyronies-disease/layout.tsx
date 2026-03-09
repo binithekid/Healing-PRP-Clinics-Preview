fcde997e11b204926216cb165121b5201ef6bbda
drsyedabdi-prog <dr.syedabdi@gmail.com>
2026-03-09 23:07:13 +0000

    Update layout.tsx

--- src/app/birmingham/peyronies-disease/layout.tsx
+++ src/app/birmingham/peyronies-disease/layout.tsx
@@ -4,9 +4,9 @@ import { ReactNode } from "react";
 export const metadata: Metadata = {
   title: {
     template: "%s | Birmingham Clinic",
-    default: "Peyronie's Disease Treatment | Birmingham Clinic",
+    default: "Doctor-Led Peyronie's Disease Treatment | Birmingham Clinic",
   },
-  description: "Specialist, non-surgical Peyronie's Disease treatments in Birmingham. Doctor-led PRP and Shockwave therapies for penile curvature and pain in Edgbaston.",
+  description: "Doctor-Led, non-surgical Peyronie's Disease treatments in Birmingham. Doctor-led PRP and Shockwave therapies for penile curvature and pain in Edgbaston.",
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
